06afff11b00686290512816a790df70375b71129
Daniel Zerlett <daniel@zerlett.eu>
2016-07-17 17:08:45 +0000

    added display

---
+++ build-all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+OPWD=$(pwd)
+
+cd $OPWD/../gopath/src/github.com/b00lduck/raspberry-datalogger-dataservice-server
+./build-docker.sh
+
+cd $OPWD/../gopath/src/github.com/b00lduck/raspberry-datalogger-serial
+./build-docker.sh 
+
+cd $OPWD/../gopath/src/github.com/b00lduck/raspberry-datalogger-acquisition
+./build-docker.sh 
+
+cd $OPWD/../gopath/src/github.com/b00lduck/raspberry-datalogger-display
+./build-docker.sh 
+
+cd $OPWD
