28504118ef1320399e891aefa505402ca2903e93
Daniel Zerlett <daniel@zerlett.eu>
2016-12-13 20:43:14 +0000

    go 1.7

--- build-all.sh
+++ build-all.sh
@@ -4,11 +4,11 @@ set -e
 
 OPWD=$(pwd)
 
+cd $OPWD/../rpi-golang-1.7
+docker build -t rem/rpi-golang-1.7:latest .
+
 cd $OPWD/../gopath/src/github.com/b00lduck/raspberry-datalogger-dataservice-server
 ./build-docker.sh
-
-cd $OPWD/../gopath/src/github.com/b00lduck/raspberry-datalogger-serial
-./build-docker.sh 
 
 cd $OPWD/../gopath/src/github.com/b00lduck/raspberry-datalogger-acquisition
 ./build-docker.sh 
